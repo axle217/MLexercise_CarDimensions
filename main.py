3b2cbfc66d18ac5f6939b7e75e660b73d6d695ba
axle217 <axle.nivek@gmail.com>
2025-09-26 20:10:20 +0300

    To test in Collab. not enough disk space in GCP free tier too install requirements and VertexAI can only contain one package, not all 3 (sklearn, tensorflow, torch). So gonna use Cloud Platform when you hire me and pay for it.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print(os.getcwd())
 from config import Config
 from data.loader import load_data
 from models.ModelsTrainer import ModelTrainer as Trainer
-from models.evaluator import ModelEvaluator
+from evaluator.evaluator import ModelEvaluator
 import torch
 from sklearn.preprocessing import StandardScaler
 from helpers.utils import plot_scatter_comparison
